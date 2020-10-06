04d32237cbf70f1fc13830f22f6c2207dea6e075
N Lloyd <nick409100@hotmail.com>
2020-10-05 19:46:29 -0700

    first commit

---
+++ AddingMachine.java
@@ -0,0 +1,35 @@
+/**
+ *  This program was made to test my Git proficiency, as well as basic functions in Java.
+ *  AddingMachine adds and subtracts integers from a variable, keeping
+ *  track of the total. Upon request, a print can be conducted.
+ *
+ *  @author Nicholas Lloyd
+ *  @version 1.0
+ *  date: 10/5/2020
+ */
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
